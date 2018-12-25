50ae0ae6e5ca13636262471f0f9dbb884fb9695a
蒋宇飞 <1800011734@pku.edu.cn>
2018-12-25 23:10:52 +0800

    Add files via upload

---
+++ pyassign4/wcount.py
@@ -0,0 +1,70 @@
+"""wcount.py: count words from an Internet file.
+
+__author__ = "Jiangyufei"
+__pkuid__  = "1800011734"
+__email__  = "1800011734@pku.edu.cn"
+"""
+
+import sys
+from urllib.request import urlopen
+import urllib.error
+
+
+'''Function "wcount" is to analyse the given lines and print the top-n words and their frequency.'''
+
+
+def wcount(lines, topn=10):
+    lst = list(map(str, lines.splitlines()))  # Turn the lines into a list of all the words which's "\n" and "\r" have been removed.
+    wlist = []
+    for i in lst:
+        if i == lst[0]:
+            wlist += i[14:len(i)-1].split()  # For the form of the first word in this txt is special, cut off 14 letters before the exact word.
+        else:
+            wlist += i[2:len(i)-1].split()  # Cut off the 'b' letter ahead and the "'" in the end.
+
+    wordlist = []
+    for word in wlist:
+        newword = ''
+        for i in word:
+            if ',.!?/()[]\/*"_:;#@$%&'.find(i) == -1 and i.isdigit() is False:  # Remove all the punctuations and numbers.
+                newword += i
+        wordlist.append(newword)
+
+    wordlist.remove('')  # Remove all the strings that have nothing inside.
+
+    result = {}
+    for word in wordlist:
+        if len(word) > 2:
+            if word[-2] == "'":  # If word[-2] is "'", the word is possessive case of noun, which need to remove the "'s" in the end.
+                word = word[:len(word)-2]
+        if word not in result:
+            result[word] = 0
+        result[word] += 1
+    sortedresult = sorted(result, key=lambda x: result[x], reverse=True)  # Sort the keys, according to values.
+    for word in sortedresult[:topn]:
+        print(word.ljust(10), str(result[word]).rjust(5))  # Adjust the alignment using ljust and rjust.
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) == 1:  # If there is no input, print out the instruction.
+        print('  Usage: {} url [topn]'.format(sys.argv[0]))
+        print('  url: URL of the txt file to analyze ')
+        print('  topn: how many (words count) to output. If not given, will output top 10 words')
+        sys.exit(1)
+
+    else:
+        try:
+            doc = urlopen(sys.argv[1])
+            docstr = doc.read().lower()
+            doc.close()
+        except urllib.error.HTTPError:  # If url wrong, print out "Wrong URL".
+            print('Wrong URL')
+        except urllib.error.URLError:  # If there is not internet connection, print out "Broken net".
+            print('Broken net')
+        else:                          # If there is no error, operate routine normally.
+            if sys.argv[2].isdigit():  # If input number after URL, choose the first number as topn.
+                topn = int(sys.argv[2])
+                wcount(docstr, topn)
+            else:                      # If there is no number input, simply call "wcount".
+                wcount(docstr)
